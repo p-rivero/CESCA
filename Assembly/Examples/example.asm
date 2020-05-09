4122125b47e518cc7fc7c547f06000c0d36cda67
p-rivero <65060696+p-rivero@users.noreply.github.com>
2020-05-09 15:02:59 +0200

    Add files via upload

--- Assembly/Examples/example.asm
+++ Assembly/Examples/example.asm
@@ -12,48 +12,48 @@ constant = 0x10 - (2 * 4 + 0x07)
 
 ; global variables (you can use #d8, #res and #str):
 #bank "data"
-num:		#d8 5
-result:		#res 1 ; reserves 1 byte
-vector:		#d8 3, 2, 1, 0, -1
-	
-	
+num:        #d8 5
+result:     #res 1 ; reserves 1 byte
+vector:     #d8 3, 2, 1, 0, -1
+    
+    
 ; program memory
 #bank "program"
 
 global_label:
-	.local_const = 2 		; labels and constants that start with a dot (.) are local (only visible whithin 2 global labels)
-	
-	NOP						; This is the first instruction that will be executed (PC=0x00). It's a NOP so we're off to a bad start :(
-	LI R3, constant			; Loads 1 into R3 (0x10 - (2 * 4 + 0x07) evaluates to 1). R3 is a protected register
-	LI R2, .local_const		; Loads 2 into R2. R2 is a protected register
-	SUB R1, R2, R3
-	DEC-Reg R1				; Outputs the result of the subtraction to the decimal display
-	JNZ .local_label
-	J another_label
-	
+    .local_const = 2        ; labels and constants that start with a dot (.) are local (only visible whithin 2 global labels)
+    
+    NOP                     ; This is the first instruction that will be executed (PC=0x00). It's a NOP so we're off to a bad start :(
+    LI R3, constant         ; Loads 1 into R3 (0x10 - (2 * 4 + 0x07) evaluates to 1). R3 is a protected register
+    LI R2, .local_const     ; Loads 2 into R2. R2 is a protected register
+    SUB R1, R2, R3
+    DEC-Reg R1              ; Outputs the result of the subtraction to the decimal display
+    JNZ .local_label
+    J another_label
+    
 .local_label:
-	LD-Addr R2, num			; Loads 5 into R2 (protected register)
-	LI R0, vector			; Loads the address of vector[0]
-	ADDI R0, R0, 2
-	LD-Reg R1, R0			; Loads 1 into R1 (vector[2])
-	JAL subroutine			; Calls a subroutine. The arguments are in R0 and R1
-	
-	LCD-Reg R0				; The returned value of the subroutine is in R0. Outputs the result to the LCD display
-	ST-Reg R2, R0			; Stores 5 into the address in R0. The value of R2 has been preserved by the subroutine
-	HLT						; Program ends
-	
-another_label:				; From this point, .local_const and .local_label aren't available anymore
-	MOVE R0, R1				; Copies the contents of R1 to R0
-	ST-Addr R2, result		; Stores the contents of R2 to the reserved space in data memory
-	DEC-Mem vector + 4		; Outputs -1 to the decimal display (vector[4])
-	J global_label
-	
+    LD-Addr R2, num         ; Loads 5 into R2 (protected register)
+    LI R0, vector           ; Loads the address of vector[0]
+    ADDI R0, R0, 2
+    LD-Reg R1, R0           ; Loads 1 into R1 (vector[2])
+    JAL subroutine          ; Calls a subroutine. The arguments are in R0 and R1
+    
+    LCD-Reg R0              ; The returned value of the subroutine is in R0. Outputs the result to the LCD display
+    ST-Reg R2, R0           ; Stores 5 into the address in R0. The value of R2 has been preserved by the subroutine
+    HLT                     ; Program ends
+    
+another_label:              ; From this point, .local_const and .local_label aren't available anymore
+    MOVE R0, R1             ; Copies the contents of R1 to R0
+    ST-Addr R2, result      ; Stores the contents of R2 to the reserved space in data memory
+    DEC-Mem vector + 4      ; Outputs -1 to the decimal display (vector[4])
+    J global_label
+    
 
 subroutine:
-	PUSH R2 				; Stores the contents of the protected registers it needs to use
-	
-	LI R2, 0xAB
-	; ... rest of the subroutine
-	
-	POP R2					; Restores protected registers
-	RET						; Returns to the point where the subroutine was called
+    PUSH R2                 ; Stores the contents of the protected registers it needs to use
+    
+    LI R2, 0xAB
+    ; ... rest of the subroutine
+    
+    POP R2                  ; Restores protected registers
+    RET                     ; Returns to the point where the subroutine was called
--- Assembly/Examples/subroutine_library.asm
+++ Assembly/Examples/subroutine_library.asm
@@ -7,83 +7,83 @@
 
 
 #bank "data"
-mult_A:		#res 2		; First operand for mult16
-mult_B:		#res 2		; Second operand for mult16
-	
+mult_A:     #res 2      ; First operand for mult16
+mult_B:     #res 2      ; Second operand for mult16
+    
 
 #bank "program"
 
-; MULTIPLY SUBROUTINE		Pre: R0 = n, R1 = m		Post: R0 = n*m
+; MULTIPLY SUBROUTINE       Pre: R0 = n, R1 = m     Post: R0 = n*m
 multiply: 
-	PUSH R2 		; Store protected register
-	MOVE R2, R0 	; Move n to R2
-	LI R0, 0		; R0 contains the sum
-	JZ .return		; if n == 0, return 0
-	
+    PUSH R2         ; Store protected register
+    MOVE R2, R0     ; Move n to R2
+    LI R0, 0        ; R0 contains the sum
+    JZ .return      ; if n == 0, return 0
+    
 .m_loop:
-	ADD R0, R0, R1	; sum += m
-	ADDI R2, R2, -1	; Decrement n
-	JNZ .m_loop
-	
+    ADD R0, R0, R1  ; sum += m
+    ADDI R2, R2, -1 ; Decrement n
+    JNZ .m_loop
+    
 .return:
-	POP R2			; Restore protected register
-	RET
-	
+    POP R2          ; Restore protected register
+    RET
+    
 
 
-; 16 BIT MULTIPLY SUBROUTINE		Pre: mult_A contains A, mult_B contains B		Post: R0/R1 = A*B
+; 16 BIT MULTIPLY SUBROUTINE        Pre: mult_A contains A, mult_B contains B       Post: R0/R1 = A*B
 ; All numbers are stored in little-endian format (R0,R2 = lower bits and R1,R3 = upper bits)
 mult16:
-	PUSH R2
-	PUSH R3
-	LI R0, 0			; R0/R1 contains the sum
-	LI R1, 0
-	LD-Addr R2, mult_B		; Load B operand to R2/R3
-	LD-Addr R3, mult_B + 1
-	
-	J .loop_cond
-	
+    PUSH R2
+    PUSH R3
+    LI R0, 0            ; R0/R1 contains the sum
+    LI R1, 0
+    LD-Addr R2, mult_B      ; Load B operand to R2/R3
+    LD-Addr R3, mult_B + 1
+    
+    J .loop_cond
+    
 .loop:
-	PUSH R2
-	PUSH R3
-	
-	LD-Addr R2, mult_A	; Load A operand to R2/R3 temporarily
-	LD-Addr R3, mult_A + 1
-	
-	ADD R0, R0, R2		; Add A to result
-	ADDC R1, R1, R3
-	
-	POP R3
-	POP R2
-	
-	ADDI R2, R2, -1		; Decrement B
-	JC .loop_cond		; Subtraction using ADDI: the carry flag is inverted!!
-	ADDI R3, R3, -1
+    PUSH R2
+    PUSH R3
+    
+    LD-Addr R2, mult_A  ; Load A operand to R2/R3 temporarily
+    LD-Addr R3, mult_A + 1
+    
+    ADD R0, R0, R2      ; Add A to result
+    ADDC R1, R1, R3
+    
+    POP R3
+    POP R2
+    
+    ADDI R2, R2, -1     ; Decrement B
+    JC .loop_cond       ; Subtraction using ADDI: the carry flag is inverted!!
+    ADDI R3, R3, -1
 .loop_cond:
-	CMP-OR R2, R3		; Test if R2/R3 (B) is 0
-	JNZ .loop			; Repeat while B != 0
+    CMP-OR R2, R3       ; Test if R2/R3 (B) is 0
+    JNZ .loop           ; Repeat while B != 0
 
-	POP R2
-	POP R3
-	RET
-	
+    POP R2
+    POP R3
+    RET
+    
 
 
 ; FAST SRL (>= 5 positions) 
 ; When an SRL has to be performed on a register 5 or more times, this method is (very slightly) more efficient.
 ; Note that the number of shifted positions (p) is hardcoded.
 
-	n = 8 - p
-	ROL R0, R0 ; Repeat this line n times
-	; ...
-	
-	ANDI R0, R0, (2**n - 1)	; 2**n is the nth power of 2
-	
-; Example (as a subroutine)		Pre: R0 = N		Post: R0 = N >> 6
+    n = 8 - p
+    ROL R0, R0 ; Repeat this line n times
+    ; ...
+    
+    ANDI R0, R0, (2**n - 1) ; 2**n is the nth power of 2
+    
+; Example (as a subroutine)     Pre: R0 = N     Post: R0 = N >> 6
 FastSRL_6:
-	ROL R0, R0
-	ROL R0, R0
+    ROL R0, R0
+    ROL R0, R0
 
-	ANDI R0, R0, 3
-	RET
-	
+    ANDI R0, R0, 3
+    RET
+    
