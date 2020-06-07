f0bf730dc71964f12c66580622a54a37ee6fc2db
privero <65060696+p-rivero@users.noreply.github.com>
2020-06-07 17:32:32 +0200

    Add files via upload

---
+++ Assembly/Examples/test_input.asm
@@ -0,0 +1,31 @@
+#include "CESCA.cpu"
+
+; Program for checking if the keyboard / keyboard controller work.
+; Reads inputs from the keyboard and displays them on the LCD screen.
+
+#bank "program"
+    LI R0, 0
+    LCD-Init
+    
+poll:
+    JNIN poll       ; Infinite loop until a key is pressed
+    
+    CMP-IN 0x80     ; Check if a special key has been pressed
+    JNZ special
+    
+    IN R0           ; Get the ASCII input
+    LCD-Reg R0      ; Display the character 
+    IN-Ack          ; Acknowledge the input so the next one can be sent
+    
+    J poll          ; Return to polling
+    
+special:
+    CMP-IN 0x20     ; Check if it's the Enter key
+    IN-Ack
+    JZ .backspace
+    HLT             ; If it's Enter, terminate program
+    
+.backspace:
+    LCD-Clr         ; If it's Backspace, clear the LCD
+    J poll
+    
