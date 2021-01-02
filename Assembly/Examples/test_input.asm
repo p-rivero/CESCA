c2edf28249532283e53013727f3e4a7af757933a
privero <65060696+p-rivero@users.noreply.github.com>
2021-01-02 17:18:07 +0100

    Improvements to input test

--- Assembly/Examples/test_input.asm
+++ Assembly/Examples/test_input.asm
@@ -4,6 +4,10 @@
 ; Program for checking if the keyboard / keyboard controller work.
 ; Reads inputs from the keyboard and displays them on the LCD screen.
 
+SPECIAL_KEY = 0x80
+RELEASED_KEY = 0x40
+ENTER_KEY = 0x20
+
 #bank program
     MOVI R0, 0
     LCD-Init
@@ -11,7 +15,7 @@
 poll:
     JNIN poll       ; Infinite loop until a key is pressed
     
-    CMP-IN 0x80     ; Check if a special key has been pressed
+    CMP-IN SPECIAL_KEY     ; Check if a special key has been pressed
     JNZ .special
     
     IN R0           ; Get the ASCII input
@@ -21,12 +25,15 @@ poll:
     J poll          ; Return to polling
     
 .special:
-    CMP-IN 0x20     ; Check if it's the Enter key
-    IN-Ack
+    CMP-IN ENTER_KEY    ; Check if it's the Enter key
     JZ ..backspace
-    HLT             ; If it's Enter, terminate program
+    CMP-IN RELEASED_KEY ; Skip if the Enter key was released
+    IN-Ack
+    JNZ poll
+    HLT             ; If Enter was pressed, terminate program
     
 ..backspace:
-    LCD-Clr         ; If it's Backspace, clear the LCD
+    IN-Ack
+    LCD-Clr         ; If Backspace was pressed or released, clear the LCD
     J poll
     
