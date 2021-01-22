d4203df51e0986a7753ce0ad481312b77dd3dd85
privero <polriverosallent@gmail.com>
2021-01-22 15:49:19 +0100

    Improvements to PRINT library

--- Assembly/Examples/PRINT.asm
+++ Assembly/Examples/PRINT.asm
@@ -9,6 +9,53 @@ PRINT:
 
 .CENTINEL = 0xFF
 
+; Prints an 8 bit signed integer
+; Arguments: R0 = n
+.int8:
+    test R0
+    jp .uint8 ; If n is positive, print as unsigned
+    
+    ; If number is strictly negative, output '-' and print absolute value
+    mov LCD, "-"
+    ; Negate number
+    mov R1, 0
+    sub R0, R1, R0
+    ; Print unsigned integer and return
+    jmp .uint8
+
+; Prints an 8 bit unsigned integer
+; Arguments: R0 = n
+.uint8:
+    push R2
+    push R3
+    mov R2, .CENTINEL
+    push R2
+    mov R2, 10
+
+    jmp ..divide_loop.check ; Check conditions before doing first iteration
+..divide_loop:
+    call MATH.UDiv8
+    push R3     ; Push R3 (remainder)
+
+...check: ; If R0 < 10, exit loop and go to end
+    cmp R2, R0          ; R2 still contains 10. If (10 <= lower bits), continue loop
+    jleu ..divide_loop
+
+; Loop ended: pop and print remainders (digits)
+..print_loop:
+    add R0, R0, "0" ; Convert to ASCII
+    mov LCD, R0
+    pop R0
+    cmp R0, .CENTINEL
+    jne ..print_loop
+    
+..end:
+    pop R3
+    pop R2
+    ret
+
+
+
 ; Prints a 16 bit signed integer
 ; Arguments: R0-R1 = n (R0 contains lower bits)
 .int16:
@@ -33,6 +80,8 @@ PRINT:
 ; todo  now n <= 6, can be printed directly
 ; todo  problem: hard to delete leading zeros
 
+; Prints a 16 bit unsigned integer
+; Arguments: R0-R1 = n (R0 contains lower bits)
 .uint16:
     push R2
     push R3
@@ -52,20 +101,7 @@ PRINT:
     jleu ..divide_loop
 
 ; Loop ended: pop and print remainders (digits)
-    add R0, R0, "0" ; Print first digit
-    mov LCD, R0
-..print_loop:
-    pop R0
-    cmp R0, .CENTINEL
-    jeq ..end
-    add R0, R0, "0"
-    mov LCD, R0
-    jmp ..print_loop
-    
-..end:
-    pop R3
-    pop R2
-    ret
+    jmp .uint8.print_loop ; Printing the characters and returning is identical in .uint8 and .uint16
 
 ; End library
 #endif
